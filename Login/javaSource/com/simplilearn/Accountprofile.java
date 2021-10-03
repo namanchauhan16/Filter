e927b268d403341975e1a6236ef06e1f64b48e28
namanchauhan16 <86224221+namanchauhan16@users.noreply.github.com>
2021-10-03 19:22:34 +0530

    Add files via upload

---
+++ Login/javaSource/com/simplilearn/Accountprofile.java
@@ -0,0 +1,42 @@
+package com.simplilearn;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Accountprofile
+ */
+public class Accountprofile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Accountprofile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().write("i am in account filter");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String userId=request.getParameter("userId");
+		System.out.println("inside filter-->"+userId);
+		if(userId!=null) {
+		doGet(request, response);
+	}
+
+}
